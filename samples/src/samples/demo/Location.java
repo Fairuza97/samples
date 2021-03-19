cc6f1a9e600a0f4a468b5e6830fee6d521d8691f
Nur Fairuza Syahira Bt Nor Hidan <syahirafairuza@gmail.com>
2021-03-19 11:19:45 +0800

    more attribute

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -2,6 +2,7 @@ package samples.demo;
 
 public class Location {
 	private String address;
+	private String time;
 
 	public String getAddress() {
 		return address;
@@ -9,5 +10,13 @@ public class Location {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	
+	public String getTime() {
+		return address;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
 	}
 }
